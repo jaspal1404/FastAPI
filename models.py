44d360484bc5e92722cb6b60f3883453f23b79c1
Jaspal.Singh <Z00CMB4@myg9l65xx7.target.com>
2023-12-07 20:46:21 -0600

    add requirements file

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Float
-
+import requests
 
 
 class Items(Base):
@@ -13,7 +13,19 @@ class Items(Base):
     category = Column(String)
     quantity = Column(Integer)
     price = Column(Float)
-    driveup_allowed = Column(Boolean)
-    shipping_allowed = Column(Boolean)
-    store_pickup_allowed = Column(Boolean)
+    delivery_method = Column(String)
     store_id = Column(String)
+
+
+def access_cart(item_id: str, delivery_method: str, quantity: int):
+
+    auth_token = 'eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI0MDAwYTY1OS1iM2Y1LTQ4YjItOWYwMi0zZGYzNjkwMmU2NWUiLCJpc3MiOiJNSTYiLCJleHAiOjE3MDIwNzA0MDQsImlhdCI6MTcwMTk4NDAwNCwianRpIjoiVEdULjUwMmZiZDNhMzFjOTRkNTZiMjQ1ZjIzOTk5YmM0M2E4LWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6IjAwQTg2MkFGLTAwQkUtNDgwNC1BNDk4LUE2NEQ5MTlFNjMwMyIsInNjbyI6ImVjb20ubm9uZSxvcGVuaWQiLCJjbGkiOiJlY29tLWlvcy0xLjAuMCIsImFzbCI6IkwifQ.dU0CLJJRm4SRIwFD_yNdJwqEPf03B0YmoyNz4QvKqsn_Fpj4YY8HUSRDHDvBZ43jC8plvKnNmVwoycsrJHZPCC5H1sW4GAGSOxicYWX5NvjV0uI_oboohHybNxXW6XwZ76lFVVrWfzxQPb6FZFpAic88-TxCN22v6XzBk8C2jmV1jSNJP-2SZI55OJOXIIe4wc4198sl-bo6eQe46pxrYWy1bTsabABguiHSxTTabSOZWxgac13KLHZSRJwSJAFDG8Fps-USh3fIGtvnRi4iJoIsdCabdqIN_XcEgPq818bIrc9Rjgq9XtjfFJo6vyaVgnaCgh7_ZNBoHatj2DIe7g'
+    headers = {'Authorization': f'Bearer {auth_token}'}
+    data = {"cart_item": {"tcin": item_id, "quantity": quantity},
+            "fulfillment": {"ship_method": delivery_method, "type": "PICKUP", "location_id": "664"},
+            "cart_type": "REGULAR", "channel_id": "91"}
+
+    url = 'https://carts.target.com/web_checkouts/v1/cart_items?iOSAppVersion=2023.49.0&key=3d4d4435710335df6435c68e19a7cf67c635a01d'
+    response = requests.post(url, json=data, headers=headers)
+
+    return response.status_code
--- routers/shopping.py
+++ routers/shopping.py
@@ -1,12 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, Path
 from pydantic import BaseModel, Field
 from starlette import status
-from models import Items
+from models import Items, access_cart
 from typing import Annotated
 from database import SessionLocal
 from sqlalchemy.orm import Session
-
-import requests
 
 router = APIRouter()
 
@@ -40,28 +38,24 @@ db_dependency = Annotated[Session, Depends(get_db)]
 @router.get("/get-item/", status_code=status.HTTP_200_OK)
 async def get_item_by_name(db: db_dependency, item_name: str, delivery_method: str, quantity: int):
 
-    print("item: " + item_name)
-    print("method: " + delivery_method)
-    print("quantity: " + str(quantity))
     item = db.query(Items).filter(Items.name == item_name).first()
     if item is None:
-        return {"response": False, "reason": "Item not found! Would you like to try another item ?"}
+        return {"response": False, "reason": "Item not found! Would you like to try again ?"}
     else:
         if item.quantity >= quantity:
-            if "item."+delivery_method+"_allowed":
-                return {"response": True, "reason": "Item available"}
+            if item.delivery_method == delivery_method:
+                return {"response": True, "reason": "Item available. Would you like to add another item ?"}
             else:
                 return {"response": False, "reason": "Item not available for " + delivery_method + ". Would you like to try a different delivery method ?"}
         else:
-            if "item." + delivery_method + "_allowed":
-                return {"response": False, "reason": "Only " + quantity + " units of the item available. Would you like to proceed ?"}
+            if item.delivery_method == delivery_method:
+                return {"response": False, "reason": "Only " + str(quantity) + " units of the item available. Would you like to proceed ?"}
             else:
-                return {"response": False, "reason": "Only " + quantity + " units of the item available, but not available for " + delivery_method + ". Would you like to try a different delivery method ?"}
+                return {"response": False, "reason": "Only " + str(quantity) + " units of the item available, but not available for " + delivery_method + ". Would you like to try a different delivery method ?"}
 
 
 @router.get("/add-to-cart/", status_code=status.HTTP_200_OK)
 async def add_to_cart(item_id: str, delivery_method: str, quantity: int):
-    auth_token = 'eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI0MDAwYTY1OS1iM2Y1LTQ4YjItOWYwMi0zZGYzNjkwMmU2NWUiLCJpc3MiOiJNSTYiLCJleHAiOjE3MDIwNzA0MDQsImlhdCI6MTcwMTk4NDAwNCwianRpIjoiVEdULjUwMmZiZDNhMzFjOTRkNTZiMjQ1ZjIzOTk5YmM0M2E4LWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsImRpZCI6IjAwQTg2MkFGLTAwQkUtNDgwNC1BNDk4LUE2NEQ5MTlFNjMwMyIsInNjbyI6ImVjb20ubm9uZSxvcGVuaWQiLCJjbGkiOiJlY29tLWlvcy0xLjAuMCIsImFzbCI6IkwifQ.dU0CLJJRm4SRIwFD_yNdJwqEPf03B0YmoyNz4QvKqsn_Fpj4YY8HUSRDHDvBZ43jC8plvKnNmVwoycsrJHZPCC5H1sW4GAGSOxicYWX5NvjV0uI_oboohHybNxXW6XwZ76lFVVrWfzxQPb6FZFpAic88-TxCN22v6XzBk8C2jmV1jSNJP-2SZI55OJOXIIe4wc4198sl-bo6eQe46pxrYWy1bTsabABguiHSxTTabSOZWxgac13KLHZSRJwSJAFDG8Fps-USh3fIGtvnRi4iJoIsdCabdqIN_XcEgPq818bIrc9Rjgq9XtjfFJo6vyaVgnaCgh7_ZNBoHatj2DIe7g'
 
     if delivery_method == "driveup":
         delivery_method = "CURBSIDE"
@@ -69,16 +63,10 @@ async def add_to_cart(item_id: str, delivery_method: str, quantity: int):
         delivery_method = "STORE_PICKUP"
     else:
         delivery_method = delivery_method
+    response_code = access_cart(item_id, delivery_method, quantity)
 
-    headers = {'Authorization': f'Bearer {auth_token}'}
-    data = {"cart_item": {"tcin": item_id, "quantity": quantity},
-            "fulfillment": {"ship_method": delivery_method, "type": "PICKUP", "location_id": "664"},
-            "cart_type": "REGULAR", "channel_id": "91"}
+    return response_code
 
-    url = 'https://carts.target.com/web_checkouts/v1/cart_items?iOSAppVersion=2023.49.0&key=3d4d4435710335df6435c68e19a7cf67c635a01d'
-    response = requests.post(url, json=data, headers=headers)
-
-    return response.status_code
 
 
 @router.post("/add-item", status_code=status.HTTP_201_CREATED)
