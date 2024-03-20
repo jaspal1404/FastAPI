5c3fd8a15d1cc4bad2bc81d09aa5692a153fc889
jaspal1404 <jaspal.singh1404@gmail.com>
2024-03-20 15:38:15 -0500

    Update models.py

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class Items(Base):
 
 def access_cart(item_id: str, delivery_method: str, quantity: int):
 
-    auth_token = "eyJraWQiOiJlYXMyIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwOWNjYTdlMS04NTYzLTRjNWItODM4Ni1lZTMyNWZmOGI0ZmMiLCJpc3MiOiJNSTYiLCJleHAiOjE3MDIwNzc4NjYsImlhdCI6MTcwMTk5MTQ2NiwianRpIjoiVEdULjUyZWRkZDYyYWYzZjQ0NjA5ODk3YmViNzRkY2JlNGY4LWwiLCJza3kiOiJlYXMyIiwic3V0IjoiRyIsInNjbyI6ImVjb20ubm9uZSxvcGVuaWQiLCJjbGkiOiJlY29tLWlvcy0xLjAuMCIsImFzbCI6IkwifQ.D3X66v7zPDA1KS5brDcHkUT-LE5tro7DYbLFG7P6PgPMaHJf2IJlLUo9c8cJoS0XMTm_rFOwyD6R3GmQ8-7AUCE53f-0GNyFnatqIidCfKOgBpe87zv8hSKAhSfXda8aClUh86DSiYTsvhIJKL3HikWMbZ17XV9e43rJn_XHTUq5UGqf2OxPF8vm5eRP3jGiRrySofxM2NPdW1ovGGxpWtKW7YfDt3HMBa_lCf1GOoYK3fDVontmd7xuImIBflCSeGzvcUW59gfVldPkyVss5aYcCNafFQ5fZOv5of0v82LSZctoTd6oaEGr60FM-QtsixHEOpp-3pVIlBLb9qhESg"
+    auth_token = ""
     headers = {'Authorization': f'Bearer {auth_token}'}
     data = {"cart_item": {"tcin": item_id, "quantity": quantity},
             "fulfillment": {"ship_method": delivery_method, "type": "PICKUP", "location_id": "2101"},
